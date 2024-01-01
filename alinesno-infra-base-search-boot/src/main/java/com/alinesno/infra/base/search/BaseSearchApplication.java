9786ecf151439fe4addc665a7f6571bc3b8409fc
switchII <luoandon@gmail.com>
2024-01-01 08:28:08 +0800

    fix:处理优化管理界面

--- alinesno-infra-base-search-boot/src/main/java/com/alinesno/infra/base/search/BaseSearchApplication.java
+++ alinesno-infra-base-search-boot/src/main/java/com/alinesno/infra/base/search/BaseSearchApplication.java
@@ -1,6 +1,7 @@
 package com.alinesno.infra.base.search;
 
 import okhttp3.*;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
@@ -14,24 +15,7 @@ import java.io.IOException;
 public class BaseSearchApplication {
 
 	public static void main(String[] args) throws IOException {
-
-		OkHttpClient client = new OkHttpClient().newBuilder()
-				.build();
-		MediaType mediaType = MediaType.parse("application/json");
-		RequestBody body = RequestBody.create(mediaType, "{\n    \"collectionName\":\"nklpbnfw\" ,\n    \"searchText\":\"罗安东.\",\n    \"topK\":9\n}");
-		Request request = new Request.Builder()
-				.url("http://alinesno-infra-base-search-boot.beta.base.infra.linesno.com/api/base/search/vectorSearch/search")
-				.method("POST", body)
-				.addHeader("User-Agent", "Apifox/1.0.0 (https://apifox.com)")
-				.addHeader("Content-Type", "application/json")
-				.addHeader("Accept", "*/*")
-				.addHeader("Host", "alinesno-infra-base-search-boot.beta.base.infra.linesno.com")
-				.addHeader("Connection", "keep-alive")
-				.build();
-		Response response = client.newCall(request).execute();
-		System.out.println(response);
-
-//		SpringApplication.run(BaseSearchApplication.class, args);
+		SpringApplication.run(BaseSearchApplication.class, args);
 	}
 
 }
