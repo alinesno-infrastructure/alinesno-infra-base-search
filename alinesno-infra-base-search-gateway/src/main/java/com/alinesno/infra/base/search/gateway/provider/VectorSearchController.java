1ff405e625233f7734eac890d2ea7284add34765
李伟(Michael) <michaelAip@gmail.com>
2024-09-02 18:02:00 -0400

    fix: 更新 VectorSearchController.java 文件，替换 Milvus 数据服务相关引用并调整搜索逻辑。

--- alinesno-infra-base-search-gateway/src/main/java/com/alinesno/infra/base/search/gateway/provider/VectorSearchController.java
+++ alinesno-infra-base-search-gateway/src/main/java/com/alinesno/infra/base/search/gateway/provider/VectorSearchController.java
@@ -2,29 +2,19 @@ package com.alinesno.infra.base.search.gateway.provider;
 
 import com.alinesno.infra.base.search.entity.VectorDatasetEntity;
 import com.alinesno.infra.base.search.service.IVectorDatasetService;
-import com.alinesno.infra.base.search.vector.dto.EmbeddingBean;
+import com.alinesno.infra.base.search.vector.DocumentVectorBean;
 import com.alinesno.infra.base.search.vector.dto.VectorSearchDto;
-import com.alinesno.infra.base.search.vector.service.IMilvusDataService;
-import com.alinesno.infra.base.search.vector.service.IMilvusSearchService;
+import com.alinesno.infra.common.facade.response.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/base/search/vectorSearch")
 public class VectorSearchController {
-
-    @Autowired
-    private IMilvusSearchService milvusService;
-
-    @Autowired
-    private IMilvusDataService milvusDataService ;
 
     @Autowired
     private IVectorDatasetService datasetService ;
@@ -36,75 +26,18 @@ public class VectorSearchController {
      * @return 包含最近邻居ID列表的ResponseEntity对象。
      */
     @PostMapping("/search")
-    public ResponseEntity<List<EmbeddingBean>> searchMilvus(@RequestBody VectorSearchDto dto) {
+    public R<List<DocumentVectorBean>> search(@RequestBody VectorSearchDto dto) {
 
         long datasetId = dto.getDatesetId() ;
         VectorDatasetEntity datasetEntity = datasetService.getById(datasetId) ;
         dto.setCollectionName(datasetEntity.getCollectionName());
 
-        List<List<Float>> vectors = milvusDataService.textToVector(dto.getSearchText()) ;
-        List<EmbeddingBean> topksList = milvusService.search(dto.getCollectionName(), vectors, dto.getTopK());
+        List<DocumentVectorBean> topksList = datasetService.search(dto);
 
-        log.debug("topksList = {}" , topksList);
+        log.debug("topsList = {}" , topksList);
 
-        return ResponseEntity.ok(topksList);
- 
-    }
+        return R.ok(topksList);
 
-    /**
-     * 处理带有过滤条件的搜索Milvus集合的HTTP POST请求，并返回最近邻居的ID列表。
-     * @param collectionName 要搜索的集合名称。
-     * @param vectors 要搜索的向量列表。
-     * @param topK 要返回的最近邻居数量。
-     * @param exp 搜索表达式。
-     * @return 包含最近邻居ID列表的ResponseEntity对象。
-     */
-    @PostMapping("/searchWithFilter")
-    public ResponseEntity<List<Long>> searchMilvusWithFilter(
-            @RequestParam("collectionName") String collectionName,
-            @RequestBody List<List<Float>> vectors,
-            @RequestParam("topK") Integer topK,
-            @RequestParam("exp") String exp) {
-
-        List<Long> topksList = milvusService.search(collectionName, vectors, topK, exp);
-
-        return ResponseEntity.ok(topksList);
-    }
-
-    /**
-     * 处理异步搜索Milvus集合的HTTP POST请求，并返回最近邻居的ID列表。
-     * @param collectionName 要搜索的集合名称。
-     * @param vectors 要搜索的向量列表。
-     * @param partitionList 要包括在搜索中的分区列表。
-     * @param topK 要返回的最近邻居数量。
-     * @return 包含最近邻居ID列表的ResponseEntity对象。
-     * @throws ExecutionException 如果搜索过程中发生执行异常。
-     * @throws InterruptedException 如果搜索过程被中断。
-     */
-    @PostMapping("/searchAsync")
-    public ResponseEntity<List<Long>> searchMilvusAsync(
-            @RequestParam("collectionName") String collectionName,
-            @RequestBody List<List<Float>> vectors,
-            @RequestParam("partitionList") List<String> partitionList,
-            @RequestParam("topK") Integer topK) throws InterruptedException, ExecutionException {
-
-        List<Long> resultIdsList = milvusService.searchAsync(collectionName, vectors, partitionList, topK);
-
-        return ResponseEntity.ok(resultIdsList);
-    }
-
-    /**
-     * 处理获取Milvus集合分区列表的HTTP GET请求，并返回分区名称列表。
-     * @param collectionName 要获取分区列表的集合名称。
-     * @return 包含分区名称列表的ResponseEntity对象。
-     */
-    @GetMapping("/partitions")
-    public ResponseEntity<List<String>> getPartitionsList(
-            @RequestParam("collectionName") String collectionName) {
-
-        List<String> partitionList = milvusService.getPartitionsList(collectionName);
-
-        return ResponseEntity.ok(partitionList);
     }
 
 }
