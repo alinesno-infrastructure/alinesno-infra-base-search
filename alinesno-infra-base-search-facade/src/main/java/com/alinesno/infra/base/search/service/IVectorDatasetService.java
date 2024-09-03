54b7f22374bd8fe0cf1d0713839f21b5c6417039
李伟(Michael) <michaelAip@gmail.com>
2024-09-03 08:18:00 -0400

    新增向量搜索功能和服务接口，包括插入数据集知识、向量搜索和创建数据集方法。

--- alinesno-infra-base-search-facade/src/main/java/com/alinesno/infra/base/search/service/IVectorDatasetService.java
+++ alinesno-infra-base-search-facade/src/main/java/com/alinesno/infra/base/search/service/IVectorDatasetService.java
@@ -2,6 +2,8 @@ package com.alinesno.infra.base.search.service;
 
 
 import com.alinesno.infra.base.search.entity.VectorDatasetEntity;
+import com.alinesno.infra.base.search.vector.DocumentVectorBean;
+import com.alinesno.infra.base.search.vector.dto.VectorSearchDto;
 import com.alinesno.infra.common.facade.services.IBaseService;
 
 import java.util.List;
@@ -16,9 +18,27 @@ public interface IVectorDatasetService extends IBaseService<VectorDatasetEntity>
 
     /**
      * 保存到知识库中
+     *
      * @param datasetId
      * @param sentenceList
+     * @param fileName
+     * @param fileType
      */
-    void insertDatasetKnowledge(Long datasetId, List<String> sentenceList);
+    void insertDatasetKnowledge(Long datasetId, List<String> sentenceList, String fileName, String fileType);
 
+    /**
+     * 向量搜索
+     *
+     * @param dto
+     * @return
+     */
+    List<DocumentVectorBean> search(VectorSearchDto dto);
+
+    /**
+     * 创建数据集
+     * @param collectionName
+     * @param description
+     * @param shardsNum
+     */
+    void buildCreateCollectionParam(String collectionName, String description, int shardsNum);
 }
