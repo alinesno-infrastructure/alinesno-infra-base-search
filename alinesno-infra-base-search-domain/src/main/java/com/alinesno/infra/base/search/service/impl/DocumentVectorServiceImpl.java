673663d2f89749276903922cc961fa613362a9d0
陈伟(Victor) <victorAip@gmail.com>
2024-09-04 05:34:00 -0400

    修复了文档向量服务实现类的导入包问题，并优化了服务接口基类引用。

--- alinesno-infra-base-search-domain/src/main/java/com/alinesno/infra/base/search/service/impl/DocumentVectorServiceImpl.java
+++ alinesno-infra-base-search-domain/src/main/java/com/alinesno/infra/base/search/service/impl/DocumentVectorServiceImpl.java
@@ -3,10 +3,9 @@ package com.alinesno.infra.base.search.service.impl;
 import com.alinesno.infra.base.search.entity.DocumentVectorEntity;
 import com.alinesno.infra.base.search.mapper.DocumentVectorMapper;
 import com.alinesno.infra.base.search.service.IDocumentVectorService;
-import com.baomidou.dynamic.datasource.annotation.DS;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 
 /**
  * 文档向量Service业务层处理
