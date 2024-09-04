7cabbaa84c0c8152db8f2ac3ebe75783d627d487
张婷(Tina) <tinaAip@gmail.com>
2024-09-04 18:49:00 -0400

    新增 ProjectEntity 实体类，包含项目名称、描述、代码及开通渠道字段。

---
+++ alinesno-infra-base-search-facade/src/main/java/com/alinesno/infra/base/search/entity/ProjectEntity.java
@@ -0,0 +1,57 @@
+package com.alinesno.infra.base.search.entity;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * <p>
+ * 应用构建实体类
+ * </p>
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@ToString
+@EqualsAndHashCode(callSuper = true)
+@Data
+@TableName("project")
+public class ProjectEntity extends InfraBaseEntity {
+
+
+    /**
+     * 项目名称
+     */
+    @TableField("project_name")
+    @ColumnType(length=32)
+    @ColumnComment("项目名称")
+    private String projectName;
+
+    /**
+     * 项目描述
+     */
+    @TableField("project_desc")
+    @ColumnType(length=256)
+    @ColumnComment("项目描述")
+    private String projectDesc;
+
+    /**
+     * 项目代码
+     */
+    @TableField("project_code")
+    @ColumnType(length=50)
+    @ColumnComment("项目代码")
+    private String projectCode;
+
+    /**
+     * 所开通渠道
+     */
+    @TableField("document_type")
+    @ColumnType(length=255)
+    @ColumnComment("所开通渠道")
+    private String documentType ;
+}
