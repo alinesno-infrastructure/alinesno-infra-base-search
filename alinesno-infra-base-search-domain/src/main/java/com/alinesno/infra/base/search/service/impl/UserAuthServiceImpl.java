baa5c14946decf7cb1fda30511638523fdb9dfca
陈伟(Victor) <victorAip@gmail.com>
2024-09-04 01:49:00 -0400

    修复了UserAuthServiceImpl类的日志记录与服务注解问题。

--- alinesno-infra-base-search-domain/src/main/java/com/alinesno/infra/base/search/service/impl/UserAuthServiceImpl.java
+++ alinesno-infra-base-search-domain/src/main/java/com/alinesno/infra/base/search/service/impl/UserAuthServiceImpl.java
@@ -3,10 +3,9 @@ package com.alinesno.infra.base.search.service.impl;
 import com.alinesno.infra.base.search.entity.UserAuthEntity;
 import com.alinesno.infra.base.search.mapper.UserAuthMapper;
 import com.alinesno.infra.base.search.service.IUserAuthService;
-import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.stereotype.Service;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 /**
  * 用户身份验证和权限控制Service业务层处理
