bd75caba9c01b1b5385feb6537eef57c6f24ecce
陈伟(Victor) <victorAip@gmail.com>
2024-09-04 04:18:00 -0400

    改进 Elasticsearch 配置，使用 Elastic 客户端库并实现多节点支持。

--- alinesno-infra-base-search-vector/src/main/java/com/alinesno/infra/base/search/vector/elasticsearch/EsConfiguration.java
+++ alinesno-infra-base-search-vector/src/main/java/com/alinesno/infra/base/search/vector/elasticsearch/EsConfiguration.java
@@ -1,12 +1,23 @@
 package com.alinesno.infra.base.search.vector.elasticsearch;
 
-import com.alinesno.infra.base.search.vector.utils.EsClientBuilder;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import com.google.common.collect.Lists;
 import lombok.Data;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 // TODO 待稳定位置
 @Configuration
@@ -30,12 +41,34 @@ public class EsConfiguration {
     private String apiKey;
 
     @Bean
-    private RestClient getRestClient() {
-        return EsClientBuilder.getRestClientInstance(
-                host ,
-                port ,
-                username,
-                password);
+    public ElasticsearchClient docqaElasticsearchClient() {
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        List<HttpHost> httpHosts = Lists.newArrayList();
+        String[] split = host.split(",");
+        for (int i = 0; i < split.length; i++) {
+            httpHosts.add(HttpHost.create(split[i]));
+        }
+        HttpHost[] httpHosts1 = httpHosts.toArray(new HttpHost[0]);
+        RestClient client = RestClient
+                .builder(httpHosts1)
+                .setHttpClientConfigCallback(httpAsyncClientBuilder ->
+                        httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider).setKeepAliveStrategy((response, context) -> 180 * 1000))
+                .build();
+
+        ElasticsearchTransport transport = new RestClientTransport(client, new JacksonJsonpMapper());
+        return new ElasticsearchClient(transport);
     }
+
+//    @Bean
+//    public RestClient getRestClient() {
+//        return EsClientBuilder.getRestClientInstance(
+//                host ,
+//                port ,
+//                username,
+//                password);
+//    }
 
 }
