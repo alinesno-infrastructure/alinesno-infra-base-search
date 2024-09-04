5f178884db83644a0df491c7ad5b6372a193785a
陈伟(Victor) <victorAip@gmail.com>
2024-09-04 09:59:00 -0400

    新增 ElasticsearchDocumentService 接口及其实现方法。

---
+++ alinesno-infra-base-search-facade/src/main/java/com/alinesno/infra/base/search/vector/service/IElasticsearchDocumentService.java
@@ -0,0 +1,37 @@
+package com.alinesno.infra.base.search.vector.service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * ElasticsearchDocumentService接口
+ */
+public interface IElasticsearchDocumentService {
+
+    /**
+     * 保存JSON对象
+     * @param indexBase
+     * @param indexType
+     */
+    void saveJsonObject(String indexBase, String indexType);
+
+    /**
+     * 查询对象并返回List信息
+     * @param indexBase
+     * @param indexType
+     * @param queryText
+     * @return
+     */
+    List<Map<String, Object>> search(String indexBase, String indexType, String queryText);
+
+    /**
+     * 精确查询对象字段并返回List信息
+     * @param indexBase
+     * @param indexType
+     * @param fieldName
+     * @param queryText
+     * @return
+     */
+    List<Map<String, Object>> search(String indexBase, String indexType, String fieldName, String queryText);
+
+}
