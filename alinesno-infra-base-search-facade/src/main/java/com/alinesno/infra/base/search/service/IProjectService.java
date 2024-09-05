67abf6122d7605710da2ead36413d09fb904c9f7
张婷(Tina) <tinaAip@gmail.com>
2024-09-04 21:03:00 -0400

    新增IProjectService接口及初始化默认应用方法。

---
+++ alinesno-infra-base-search-facade/src/main/java/com/alinesno/infra/base/search/service/IProjectService.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.base.search.service;
+
+import com.alinesno.infra.base.search.entity.ProjectEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ * 应用构建Service接口
+ *
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+public interface IProjectService extends IBaseService<ProjectEntity> {
+
+    /**
+     * 初始化默认应用
+     * @param userId
+     */
+    void initDefaultApp(long userId);
+
+}
