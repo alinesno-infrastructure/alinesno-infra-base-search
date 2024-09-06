e31504fcab08bf2fa35d163b91fed1c798912590
王磊(Levi) <leviAip@gmail.com>
2024-09-05 21:47:00 -0400

    新增文档索引Service接口IDocumentIndexService。

---
+++ alinesno-infra-base-search-facade/src/main/java/com/alinesno/infra/base/search/service/IDocumentIndexService.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.search.service;
+
+import com.alinesno.infra.base.search.entity.DocumentIndexEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ * 文档索引Service接口
+ *
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+
+public interface IDocumentIndexService extends IBaseService<DocumentIndexEntity> {
+}
