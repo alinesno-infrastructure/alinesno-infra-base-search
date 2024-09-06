ea6977044a73a6b9c7c140cf05bb426a781ab69c
王磊(Levi) <leviAip@gmail.com>
2024-09-05 21:07:00 -0400

    优化配置类，添加向量数据集服务注入及命令行启动参数日志打印功能。

--- alinesno-infra-base-search-boot/src/main/java/com/alinesno/infra/base/search/config/AppConfiguration.java
+++ alinesno-infra-base-search-boot/src/main/java/com/alinesno/infra/base/search/config/AppConfiguration.java
@@ -1,21 +1,29 @@
 package com.alinesno.infra.base.search.config;
 
+import com.alinesno.infra.base.search.service.IVectorDatasetService;
 import com.alinesno.infra.common.facade.enable.EnableActable;
 import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import jakarta.servlet.MultipartConfigElement;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.util.unit.DataSize;
 
+@Slf4j
 @EnableScheduling
 @EnableActable
 @EnableInfraSsoApi
 @MapperScan("com.alinesno.infra.base.search.mapper")
 @Configuration
-public class AppConfiguration {
+public class AppConfiguration implements CommandLineRunner {
+
+    @Autowired
+    private IVectorDatasetService vectorDatasetService ;
 
     /**
      * 配置上传文件大小
@@ -29,5 +37,10 @@ public class AppConfiguration {
         factory.setMaxRequestSize(DataSize.ofMegabytes(100));
 
         return factory.createMultipartConfig();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("当前向量存储引擎:{}" , vectorDatasetService.getVectorEngine());
     }
 }
