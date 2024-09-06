0620d2ea19bd89848f4b8e5285b901279046a652
林晓(Lily) <lilyAip@gmail.com>
2024-09-06 00:22:00 -0400

    创建 base_search_ui API 文件，包含数据库接口操作。

---
+++ alinesno-infra-base-search-ui/src/api/base/search/documents.js
@@ -0,0 +1,96 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+/**
+ * 数据库接口操作
+ * 
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+
+// 接口配置项
+var prefix = '/api/infra/base/search/documents/' ;
+
+var managerUrl = {
+    datatables : prefix +"datatables" ,
+    createUrl: prefix + 'add' ,
+    saveUrl: prefix + 'save' ,
+    updateUrl: prefix +"modify" ,
+    statusUrl: prefix +"changeStatus" ,
+    cleanUrl: prefix + "clean",
+    detailUrl: prefix +"detail",
+    removeUrl: prefix + "delete" ,
+    exportUrl: prefix + "exportExcel",
+    changeField: prefix + "changeField",
+    defaultDocument: prefix + "defaultDocument",
+    searchHits: prefix + "searchHits",
+    downloadFile: prefix + "downloadFile"
+}
+
+// 搜索
+export function getSearchHits() {
+  return request({
+    url: managerUrl.searchHits ,
+    method: 'get'
+  })
+}
+
+// 获取默认应用
+export function getDefaultDocument(){
+  return request({
+    url: managerUrl.defaultDocument ,
+    method: 'get'
+  })
+}
+
+// 修改字段
+export function changStatusField(data){
+  return request({
+    url: managerUrl.changeField ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 查询数据库列表
+export function listDocument(query) {
+  return request({
+    url: managerUrl.datatables ,
+    method: 'post',
+    params: query
+  })
+}
+
+// 查询数据库详细
+export function getDocument(databaseId) {
+  return request({
+    url: managerUrl.detailUrl + '/' + parseStrEmpty(databaseId),
+    method: 'get'
+  })
+}
+
+// 新增数据库
+export function addDocument(data) {
+  return request({
+    url: managerUrl.saveUrl ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改数据库
+export function updateDocument(data) {
+  return request({
+    url: managerUrl.updateUrl ,
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除数据库
+export function delDocument(databaseId) {
+  return request({
+    url: managerUrl.removeUrl + '/' + parseStrEmpty(databaseId),
+    method: 'delete'
+  })
+}
