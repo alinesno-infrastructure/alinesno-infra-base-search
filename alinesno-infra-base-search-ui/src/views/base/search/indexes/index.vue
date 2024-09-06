0610c2b17b69595a3668a21d417a3db6ae853022
王明(AllenQin) <allenQin@gmail.com>
2024-09-06 04:34:00 -0400

    修复并更新了索引视图页面的搜索条件和按钮组件。
    修复了索引视图页面在更新后的功能问题，将应用名称搜索条件更改为索引基础名称，并优化了按钮组件的显示逻辑。

--- alinesno-infra-base-search-ui/src/views/base/search/indexes/index.vue
+++ alinesno-infra-base-search-ui/src/views/base/search/indexes/index.vue
@@ -4,20 +4,14 @@
         <!--应用数据-->
         <el-col :span="24" :xs="24">
            <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="100px">
-              <el-form-item label="应用名称" prop="name">
-                 <el-input v-model="queryParams.name" placeholder="请输入应用名称" clearable style="width: 240px" @keyup.enter="handleQuery" />
+              <el-form-item label="索引基础名称" prop="indexBase">
+                 <el-input v-model="queryParams.indexBase" placeholder="请输入索引基础名称" clearable style="width: 240px" @keyup.enter="handleQuery" />
               </el-form-item>
-              <el-form-item label="应用名称" prop="name">
-                 <el-input v-model="queryParams['condition[name|like]']" placeholder="请输入应用名称" clearable style="width: 240px" @keyup.enter="handleQuery" />
-              </el-form-item>
-              <el-form-item>
-                 <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
-                 <el-button icon="Refresh" @click="resetQuery">重置</el-button>
-              </el-form-item>
+              <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+              <el-button icon="Refresh" @click="resetQuery">重置</el-button>
            </el-form>
 
            <el-row :gutter="10" class="mb8">
-
               <el-col :span="1.5">
                  <el-button type="primary" plain icon="Plus" @click="handleAdd">新增</el-button>
               </el-col>
@@ -27,63 +21,35 @@
               <el-col :span="1.5">
                  <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete">删除</el-button>
               </el-col>
-
               <right-toolbar v-model:showSearch="showSearch" @queryTable="getList" :columns="columns"></right-toolbar>
            </el-row>
 
-           <el-table v-loading="loading" :data="ApplicationList" @selection-change="handleSelectionChange">
+           <el-table v-loading="loading" :data="DocumentIndexList" @selection-change="handleSelectionChange">
               <el-table-column type="selection" width="50" align="center" />
-
-              <el-table-column label="图标" align="center" width="80px" prop="icon" v-if="columns[0].visible">
-                 <template #default="scope">
-                 <div class="role-icon">
-                    <img :src="'http://data.linesno.com/icons/dataset/dataset_' + (scope.$index + 8) + '.png'" style="width:40px;height:40px;border-radius: 50%;" />
-                 </div>
-                 </template>
-              </el-table-column>
-
-              <!-- 业务字段-->
-              <el-table-column label="应用名称" align="left" key="name" prop="name" v-if="columns[0].visible" >
+              <el-table-column label="索引名称" align="left" key="indexBase" prop="indexBase" v-if="columns[0].visible">
                  <template #default="scope">
                     <div style="font-size: 15px;font-weight: 500;color: #3b5998;">
                        {{ scope.row.showName }}
                     </div>
                     <div style="font-size: 13px;color: #a5a5a5;">
-                       {{ scope.row.name }}
+                       {{ scope.row.indexBase }}
                     </div>
                  </template>
               </el-table-column>
-              <el-table-column label="所属领域" align="center" key="domain" prop="domain" v-if="columns[3].visible" :show-overflow-tooltip="true" />
-              <el-table-column label="应用密钥" align="center" key="appKey" prop="appKey" v-if="columns[1].visible" :show-overflow-tooltip="true" >
-                 <template #default="scope">
-                    {{ scope.row.appKey }} &nbsp;&nbsp; <el-button type="primary" text bg icon="CopyDocument">复制</el-button>
-                 </template>
-              </el-table-column>
-              <el-table-column label="状态" align="center" key="hasStatus" v-if="columns[5].visible" width="200">
-                 <template #default="scope">
-                    <el-button type="primary" text bg icon="Check">正常</el-button>
-                 </template>
-              </el-table-column>
-
-              <el-table-column label="添加时间" align="center" prop="addTime" v-if="columns[6].visible" width="160">
-                 <template #default="scope">
-                    <span>{{ parseTime(scope.row.addTime) }}</span>
-                 </template>
-              </el-table-column>
-
-              <!-- 操作字段  -->
+              <el-table-column label="索引类型" align="center" key="indexType" prop="indexType" v-if="columns[2].visible" :show-overflow-tooltip="true" />
+              <el-table-column label="分片数" align="center" key="shardNum" prop="shardNum" v-if="columns[4].visible" :show-overflow-tooltip="true" />
+              <el-table-column label="副本数" align="center" key="replicaNum" prop="replicaNum" v-if="columns[5].visible" :show-overflow-tooltip="true" />
+              <el-table-column label="文档数量" align="center" key="docCount" prop="docCount" v-if="columns[6].visible" :show-overflow-tooltip="true" />
+              <el-table-column label="存储数据量" align="center" key="storageSize" prop="storageSize" v-if="columns[8].visible" :show-overflow-tooltip="true" />
               <el-table-column label="操作" align="center" width="150" class-name="small-padding fixed-width">
                  <template #default="scope">
-                    <el-tooltip content="修改" placement="top" v-if="scope.row.ApplicationId !== 1">
-                       <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"
-                          v-hasPermi="['system:Application:edit']"></el-button>
+                    <el-tooltip content="修改" placement="top" v-if="scope.row.DocumentIndexId !== 1">
+                       <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['system:DocumentIndex:edit']"></el-button>
                     </el-tooltip>
-                    <el-tooltip content="删除" placement="top" v-if="scope.row.ApplicationId !== 1">
-                       <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"
-                          v-hasPermi="['system:Application:remove']"></el-button>
+                    <el-tooltip content="删除" placement="top" v-if="scope.row.DocumentIndexId !== 1">
+                       <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['system:DocumentIndex:remove']"></el-button>
                     </el-tooltip>
                  </template>
-
               </el-table-column>
            </el-table>
            <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum" v-model:limit="queryParams.pageSize" @pagination="getList" />
@@ -95,33 +61,67 @@
         <el-form :model="form" :rules="rules" ref="databaseRef" label-width="100px">
            <el-row>
               <el-col :span="24">
-                 <el-form-item label="应用全名" prop="name">
-                    <el-input v-model="form.name" placeholder="请输入应用名称" maxlength="50" />
+                 <el-form-item label="索引基础名称" prop="indexBase">
+                    <el-input v-model="form.indexBase" placeholder="请输入索引基础名称" maxlength="255" />
                  </el-form-item>
               </el-col>
            </el-row>
            <el-row>
               <el-col :span="24">
-                 <el-form-item label="简称" prop="showName">
-                    <el-input v-model="form.showName" placeholder="请输入类型" maxlength="50" />
+                 <el-form-item label="字段名称" prop="fieldName">
+                    <el-input v-model="form.fieldName" placeholder="请输入字段名称" maxlength="255" />
                  </el-form-item>
               </el-col>
            </el-row>
            <el-row>
               <el-col :span="24">
-                 <el-form-item label="所属领域" prop="domain">
-                    <el-input v-model="form.domain" placeholder="请输入所属领域" maxlength="128" />
+                 <el-form-item label="索引类型" prop="indexType">
+                    <el-input v-model="form.indexType" placeholder="请输入索引类型" maxlength="255" />
                  </el-form-item>
               </el-col>
            </el-row>
            <el-row>
               <el-col :span="24">
-                 <el-form-item label="应用描述" prop="remark">
-                    <el-input v-model="form.remark" placeholder="请输入连接用户名" maxlength="30" />
+                 <el-form-item label="文档ID" prop="docId">
+                    <el-input v-model="form.docId" placeholder="请输入文档ID" maxlength="255" />
                  </el-form-item>
               </el-col>
            </el-row>
-
+           <el-row>
+              <el-col :span="24">
+                 <el-form-item label="分片数" prop="shardNum">
+                    <el-input-number v-model="form.shardNum" controls-position="right" :min="1" :max="100" />
+                 </el-form-item>
+              </el-col>
+           </el-row>
+           <el-row>
+              <el-col :span="24">
+                 <el-form-item label="副本数" prop="replicaNum">
+                    <el-input-number v-model="form.replicaNum" controls-position="right" :min="0" :max="100" />
+                 </el-form-item>
+              </el-col>
+           </el-row>
+           <el-row>
+              <el-col :span="24">
+                 <el-form-item label="文档数量" prop="docCount">
+                    <el-input-number v-model="form.docCount" controls-position="right" :min="0" :max="9007199254740991" />
+                 </el-form-item>
+              </el-col>
+           </el-row>
+           <el-row>
+              <el-col :span="24">
+                 <el-form-item label="别名" prop="alias">
+                    <el-input v-model="form.alias" placeholder="请输入别名" maxlength="255" />
+                 </el-form-item>
+              </el-col>
+           </el-row>
+           <el-row>
+              <el-col :span="24">
+                 <el-form-item label="存储数据量" prop="storageSize">
+                    <el-input-number v-model="form.storageSize" controls-position="right" :min="0" :max="9007199254740991" />
+                 </el-form-item>
+              </el-col>
+           </el-row>
         </el-form>
         <template #footer>
            <div class="dialog-footer">
@@ -130,25 +130,24 @@
            </div>
         </template>
      </el-dialog>
-
   </div>
 </template>
 
-<script setup name="Application">
+<script setup name="DocumentIndex">
 
 import {
-  listApplication,
-  delApplication,
-  getApplication,
-  updateApplication,
-  addApplication
-} from "@/api/base/search/application";
+  listDocumentIndex,
+  delDocumentIndex,
+  getDocumentIndex,
+  updateDocumentIndex,
+  addDocumentIndex
+} from "@/api/base/search/documentIndex";
 
 const router = useRouter();
 const { proxy } = getCurrentInstance();
 
 // 定义变量
-const ApplicationList = ref([]);
+const DocumentIndexList = ref([]);
 const open = ref(false);
 const loading = ref(true);
 const showSearch = ref(true);
@@ -169,7 +168,10 @@ const columns = ref([
   { key: 3, label: `类型`, visible: true },
   { key: 4, label: `应用地址`, visible: true },
   { key: 5, label: `状态`, visible: true },
-  { key: 6, label: `更新时间`, visible: true }
+  { key: 6, label: `更新时间`, visible: true },
+  { key: 7, label: `更新时间`, visible: true },
+  { key: 8, label: `更新时间`, visible: true },
+  { key: 9, label: `更新时间`, visible: true }
 ]);
 
 const data = reactive({
@@ -194,9 +196,9 @@ const { queryParams, form, rules } = toRefs(data);
 /** 查询应用列表 */
 function getList() {
   loading.value = true;
-  listApplication(proxy.addDateRange(queryParams.value, dateRange.value)).then(res => {
+  listDocumentIndex(proxy.addDateRange(queryParams.value, dateRange.value)).then(res => {
      loading.value = false;
-     ApplicationList.value = res.rows;
+     DocumentIndexList.value = res.rows;
      total.value = res.total;
   });
 };
@@ -217,9 +219,9 @@ function resetQuery() {
 };
 /** 删除按钮操作 */
 function handleDelete(row) {
-  const ApplicationIds = row.id || ids.value;
-  proxy.$modal.confirm('是否确认删除应用编号为"' + ApplicationIds + '"的数据项？').then(function () {
-     return delApplication(ApplicationIds);
+  const DocumentIndexIds = row.id || ids.value;
+  proxy.$modal.confirm('是否确认删除应用编号为"' + DocumentIndexIds + '"的数据项？').then(function () {
+     return delDocumentIndex(DocumentIndexIds);
   }).then(() => {
      getList();
      proxy.$modal.msgSuccess("删除成功");
@@ -238,7 +240,7 @@ function reset() {
   form.value = {
      id: undefined,
      deptId: undefined,
-     ApplicationName: undefined,
+     DocumentIndexName: undefined,
      nickName: undefined,
      password: undefined,
      phonenumber: undefined,
@@ -263,8 +265,8 @@ function handleAdd() {
 /** 修改按钮操作 */
 function handleUpdate(row) {
   reset();
-  const ApplicationId = row.id || ids.value;
-  getApplication(ApplicationId).then(response => {
+  const DocumentIndexId = row.id || ids.value;
+  getDocumentIndex(DocumentIndexId).then(response => {
      form.value = response.data;
      open.value = true;
      title.value = "修改应用";
@@ -276,13 +278,13 @@ function submitForm() {
   proxy.$refs["databaseRef"].validate(valid => {
      if (valid) {
         if (form.value.id != undefined) {
-           updateApplication(form.value).then(response => {
+           updateDocumentIndex(form.value).then(response => {
               proxy.$modal.msgSuccess("修改成功");
               open.value = false;
               getList();
            });
         } else {
-           addApplication(form.value).then(response => {
+           addDocumentIndex(form.value).then(response => {
               proxy.$modal.msgSuccess("新增成功");
               open.value = false;
               getList();
