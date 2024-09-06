3e1150f4728b4294b721d2c909a78cc1d1aa9140
王明(AllenQin) <allenQin@gmail.com>
2024-09-05 21:02:00 -0400

    新增 field.vue 文件，实现基础搜索 UI 的部门选择功能。

---
+++ alinesno-infra-base-search-ui/src/views/base/search/document/field.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="head-container">
+            <el-input
+                v-model="deptName"
+                placeholder="请输入类型名称"
+                clearable
+                prefix-icon="Search"
+                style="margin-bottom: 20px"
+            />
+        </div>
+        <div class="head-container">
+            <el-tree
+                :data="deptOptions"
+                :props="{ label: 'label', children: 'children' }"
+                :expand-on-click-node="false"
+                :filter-node-method="filterNode"
+                ref="deptTreeRef"
+                node-key="id"
+                highlight-current
+                @node-click="handleNodeClick"
+            />
+                <!-- default-expand-all -->
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+</style>
+
+
