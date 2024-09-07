b7852d88113f13c744de2410292c73eed3f1d2fa
李娜(Allen) <allenAip@gmail.com>
2024-09-07 02:45:00 -0400

    修复 VectorSearchDto 类中数据集ID和搜索文本的验证逻辑，并添加集合名称字段。

--- alinesno-infra-base-search-facade/src/main/java/com/alinesno/infra/base/search/vector/dto/VectorSearchDto.java
+++ alinesno-infra-base-search-facade/src/main/java/com/alinesno/infra/base/search/vector/dto/VectorSearchDto.java
@@ -1,12 +1,18 @@
 package com.alinesno.infra.base.search.vector.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class VectorSearchDto {
 
-    private long datesetId ; // 数据集ID
+    @NotNull(message = "数据集ID不能为空")
+    private Long datesetId ; // 数据集ID
+
     private String collectionName ;
+
+    @NotBlank(message = "搜索文本不能为空")
     private String searchText ;
     private Integer topK ;
 
