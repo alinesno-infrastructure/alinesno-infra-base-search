44a9626d65268f61c21dea8142ae2b33c6b85226
李娜(Allen) <allenAip@gmail.com>
2024-09-07 05:31:00 -0400

    feat:实现文档向量服务接口，包含索引创建逻辑及信息保存

--- alinesno-infra-base-search-domain/src/main/java/com/alinesno/infra/base/search/service/impl/DocumentIndexServiceImpl.java
+++ alinesno-infra-base-search-domain/src/main/java/com/alinesno/infra/base/search/service/impl/DocumentIndexServiceImpl.java
@@ -1,11 +1,18 @@
 package com.alinesno.infra.base.search.service.impl;
 
+import com.alinesno.infra.base.search.api.DocumentIndexDto;
+import com.alinesno.infra.base.search.api.IndexInfoDto;
 import com.alinesno.infra.base.search.entity.DocumentIndexEntity;
 import com.alinesno.infra.base.search.mapper.DocumentIndexMapper;
 import com.alinesno.infra.base.search.service.IDocumentIndexService;
+import com.alinesno.infra.base.search.vector.service.IElasticsearchDocumentService;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 文档向量Service业务层处理
@@ -13,8 +20,24 @@ import org.springframework.stereotype.Service;
  * @author luoxiaodong
  * @version 1.0.0
  */
-@Service
 @Slf4j
+@Service
 public class DocumentIndexServiceImpl extends IBaseServiceImpl<DocumentIndexEntity, DocumentIndexMapper> implements IDocumentIndexService {
 
+    @Autowired
+    private IElasticsearchDocumentService elasticsearchDocumentService;
+
+    @Override
+    public void createDocumentIndex(DocumentIndexDto dto) {
+
+        log.info("创建文档索引：{}", dto);
+        String indexName = elasticsearchDocumentService.generateIndexName(dto.getIndexBase(), dto.getIndexType()) ;
+        elasticsearchDocumentService.createDocumentIndex(indexName, null) ;
+
+        // 保存索引信息
+        DocumentIndexEntity documentIndexEntity = new DocumentIndexEntity() ;
+        BeanUtils.copyProperties(dto, documentIndexEntity);
+
+        this.save(documentIndexEntity);
+    }
 }
