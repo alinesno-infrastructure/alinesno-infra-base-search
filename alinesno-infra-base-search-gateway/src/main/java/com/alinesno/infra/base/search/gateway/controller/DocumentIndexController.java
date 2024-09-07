405b50ecf5e729715f4e0914e7149b97d8e62978
李娜(Allen) <allenAip@gmail.com>
2024-09-07 10:02:00 -0400

    优化 DocumentIndexController，引入 DocumentIndexDto 和 IndexInfoDto，并集成 ElasticsearchDocumentService。

--- alinesno-infra-base-search-gateway/src/main/java/com/alinesno/infra/base/search/gateway/controller/DocumentIndexController.java
+++ alinesno-infra-base-search-gateway/src/main/java/com/alinesno/infra/base/search/gateway/controller/DocumentIndexController.java
@@ -1,10 +1,14 @@
 package com.alinesno.infra.base.search.gateway.controller;
 
+import com.alinesno.infra.base.search.api.DocumentIndexDto;
+import com.alinesno.infra.base.search.api.IndexInfoDto;
 import com.alinesno.infra.base.search.entity.DocumentIndexEntity;
 import com.alinesno.infra.base.search.service.IDocumentIndexService;
+import com.alinesno.infra.base.search.vector.service.IElasticsearchDocumentService;
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
+import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.rest.BaseController;
 import io.swagger.annotations.Api;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,10 +17,9 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 处理与DocumentVectorEntity相关的请求的Controller。
@@ -35,6 +38,9 @@ public class DocumentIndexController extends BaseController<DocumentIndexEntity,
     @Autowired
     private IDocumentIndexService service;
 
+    @Autowired
+    private IElasticsearchDocumentService elasticsearchDocumentService;
+
     /**
      * 获取DocumentVectorEntity的DataTables数据。
      *
@@ -47,7 +53,52 @@ public class DocumentIndexController extends BaseController<DocumentIndexEntity,
     @PostMapping("/datatables")
     public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
         log.debug("page = {}", ToStringBuilder.reflectionToString(page));
-        return this.toPage(model, this.getFeign(), page);
+
+        TableDataInfo tableDataInfo = this.toPage(model, this.getFeign(), page);
+
+        List<DocumentIndexEntity> indexInfoList = (List<DocumentIndexEntity>) tableDataInfo.getRows();
+        if(indexInfoList != null){
+            indexInfoList.forEach(e -> {
+
+                long totalSize = 0L;
+                long documentSize = 0L;
+                long indexCount = 0L;
+
+                // 查询索引信息
+                List<IndexInfoDto> dtoList = null;
+
+                try {
+                    dtoList = elasticsearchDocumentService.getIndexInfo(e.getIndexBase());
+
+                    totalSize = dtoList.stream().mapToLong(IndexInfoDto::getStorageSize).sum();
+                    documentSize = dtoList.stream().mapToLong(IndexInfoDto::getDocCount).sum();
+                    indexCount = dtoList.size();
+
+                } catch (Exception ex) {
+                    log.error("查询索引信息失败", ex);
+                }
+
+                e.setInfoList(dtoList);
+                e.setStorageSize(totalSize);
+                e.setDocCount(documentSize);
+                e.setIndexCount(indexCount);
+            });
+        }
+
+        tableDataInfo.setRows(indexInfoList);
+
+        return tableDataInfo;
+
+    }
+
+    /**
+     * 创建索引
+     * @return
+     */
+    @PostMapping("/createDocumentIndex")
+    public AjaxResult createDocumentIndex(@RequestBody DocumentIndexDto dto){
+        service.createDocumentIndex(dto);
+        return ok() ;
     }
 
     @Override
