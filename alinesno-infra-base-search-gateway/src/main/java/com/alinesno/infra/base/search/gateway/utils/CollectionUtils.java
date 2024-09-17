c917ffc29cd817e3a7b8ae313aacb126af83e2a2
李伟(Michael) <michaelAip@gmail.com>
2024-09-17 11:59:02 +0800

    feat: 更新 CollectionUtils 类，添加唯一集合名称生成与随机字符串生成方法。

--- alinesno-infra-base-search-gateway/src/main/java/com/alinesno/infra/base/search/gateway/utils/CollectionUtils.java
+++ alinesno-infra-base-search-gateway/src/main/java/com/alinesno/infra/base/search/gateway/utils/CollectionUtils.java
@@ -1,24 +1,46 @@
-//package com.alinesno.infra.base.search.gateway.utils;
-//
-//import java.security.SecureRandom;
-//
-///**
-// * 集成工具类
-// */
-//public class CollectionUtils {
-//
-//    public static String generateUniqueString() {
-//        int length = 8;
-//        String characters = "abcdefghijklmnopqrstuvwxyz";
-//        SecureRandom random = new SecureRandom();
-//        StringBuilder sb = new StringBuilder();
-//
-//        for (int i = 0; i < length; i++) {
-//            int randomIndex = random.nextInt(characters.length());
-//            sb.append(characters.charAt(randomIndex));
-//        }
-//
-//        return sb.toString();
-//    }
-//
-//}
+package com.alinesno.infra.base.search.gateway.utils;
+
+import com.alinesno.infra.base.search.entity.VectorDatasetEntity;
+import com.alinesno.infra.base.search.service.IVectorDatasetService;
+import com.alinesno.infra.common.web.log.utils.SpringUtils;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+
+import java.util.*;
+
+public class CollectionUtils {
+
+    /**
+     * // TODO 待优化
+     * 确定唯一值，不重复
+     */
+    public static String getCollectionName() {
+
+        String collectionName = generateRandomString(10);
+
+        IVectorDatasetService vectorDatasetService = SpringUtils.getBean(IVectorDatasetService.class);
+        LambdaQueryWrapper<VectorDatasetEntity> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(VectorDatasetEntity::getCollectionName, collectionName);
+        long count = vectorDatasetService.count(queryWrapper);
+
+        if (count > 0) {
+            return getCollectionName();
+        }
+
+        return collectionName;
+    }
+
+
+    // TODO 待处理确认当前为系统唯一值
+    public static String generateRandomString(int length) {
+
+        String allowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(allowedChars.length());
+            sb.append(allowedChars.charAt(index));
+        }
+        return sb.toString();
+    }
+}
