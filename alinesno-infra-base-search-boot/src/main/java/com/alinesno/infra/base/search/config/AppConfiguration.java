35ac893140aab4195618d5cd669bd97ba74fb6aa
陈伟(Victor) <victorAip@gmail.com>
2024-11-02 12:51:13 +0800

    feat:在 AppConfiguration 类中添加 ForestScan 注解

--- alinesno-infra-base-search-boot/src/main/java/com/alinesno/infra/base/search/config/AppConfiguration.java
+++ alinesno-infra-base-search-boot/src/main/java/com/alinesno/infra/base/search/config/AppConfiguration.java
@@ -4,6 +4,7 @@ import com.alinesno.infra.base.search.service.IVectorDatasetService;
 import com.alinesno.infra.common.facade.enable.EnableActable;
 import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import com.alinesno.infra.common.web.log.aspect.LogAspect;
+import com.dtflys.forest.springboot.annotation.ForestScan;
 import jakarta.servlet.MultipartConfigElement;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
@@ -20,6 +21,10 @@ import org.springframework.util.unit.DataSize;
 @EnableActable
 @EnableInfraSsoApi
 @MapperScan("com.alinesno.infra.base.search.mapper")
+@ForestScan({
+        "com.alinesno.infra.common.web.adapter.base.consumer" ,
+        "com.alinesno.infra.base.search.adapter.consumer"
+})
 @Configuration
 public class AppConfiguration implements CommandLineRunner {
 
