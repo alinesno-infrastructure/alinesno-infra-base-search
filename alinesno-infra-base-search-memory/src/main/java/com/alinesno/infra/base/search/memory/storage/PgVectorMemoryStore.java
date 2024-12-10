e501e942738c8cc1194c9b4d5c97353d61f5e964
张婷(Tina) <tinaAip@gmail.com>
2024-12-10 22:39:45 +0800

    feat:新增 PgVectorMemoryStore 类，实现 BaseMemoryStore 接口。

---
+++ alinesno-infra-base-search-memory/src/main/java/com/alinesno/infra/base/search/memory/storage/PgVectorMemoryStore.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.base.search.memory.storage;
+
+import com.alinesno.infra.base.search.memory.BaseMemoryStore;
+
+/**
+ */
+public class PgVectorMemoryStore implements BaseMemoryStore {
+}
