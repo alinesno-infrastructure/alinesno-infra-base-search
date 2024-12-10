552b73037c09771be86a9aa40a2901754871bc84
张婷(Tina) <tinaAip@gmail.com>
2024-12-10 22:39:45 +0800

    feat:新增 MemoryService 接口，定义记忆系统的基本操作。

---
+++ alinesno-infra-base-search-memory/src/main/java/com/alinesno/infra/base/search/memory/service/MemoryService.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.base.search.memory.service;
+
+import com.alinesno.infra.base.search.memory.MemoryBase;
+
+/**
+ * Memory 接口定义了一个记忆系统的操作规范，包括添加、获取和列出记忆。
+ */
+public interface MemoryService extends MemoryBase {
+
+}
