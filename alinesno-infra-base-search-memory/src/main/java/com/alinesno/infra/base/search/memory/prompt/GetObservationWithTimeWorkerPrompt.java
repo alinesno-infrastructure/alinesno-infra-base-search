c189cdb3560ebe0a9fd50ac32efe44f23287c23e
李伟(Michael) <michaelAip@gmail.com>
2025-02-08 00:14:14 +0800

    feat:新增 GetObservationWithTimeWorkerPrompt 类的方法以生成不同格式的提示语

--- alinesno-infra-base-search-memory/src/main/java/com/alinesno/infra/base/search/memory/prompt/GetObservationWithTimeWorkerPrompt.java
+++ alinesno-infra-base-search-memory/src/main/java/com/alinesno/infra/base/search/memory/prompt/GetObservationWithTimeWorkerPrompt.java
@@ -10,8 +10,26 @@ import org.springframework.context.annotation.Configuration;
 public class GetObservationWithTimeWorkerPrompt {
 
     private String time_string_format ;
+    private String get_observation_with_time_system ;
     private String get_observation_with_time_few_shot ;
     private String get_observation_with_time_user_query ;
 
+    public String getObservationWithTimePrompt(String userName, String numObs) {
+        return getGet_observation_with_time_system()
+                .replace("{user_name}", userName)
+                .replace("{num_obs}", numObs);
+    }
 
+
+    public String getObservationWithTimeFewShot(String userName) {
+        return getGet_observation_with_time_few_shot()
+                .replace("{user_name}", userName);
+    }
+
+
+    public String getObservationWithTimeUserQuery(String userName, String userQuery) {
+        return getGet_observation_with_time_user_query()
+                .replace("{user_name}", userName)
+                .replace("{user_query}", userQuery);
+    }
 }
