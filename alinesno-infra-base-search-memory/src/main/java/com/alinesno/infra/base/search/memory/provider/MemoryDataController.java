8f089b33ddd76818e0ba7a74bf67e514bbe360bc
李伟(Michael) <michaelAip@gmail.com>
2025-02-08 00:14:14 +0800

    feat:新增 MemoryDataController 控制器类，实现记忆库的增删查功能。

---
+++ alinesno-infra-base-search-memory/src/main/java/com/alinesno/infra/base/search/memory/provider/MemoryDataController.java
@@ -0,0 +1,71 @@
+package com.alinesno.infra.base.search.memory.provider;
+
+import com.alinesno.infra.base.search.memory.IMemoryBase;
+import com.alinesno.infra.base.search.memory.bean.AgentMemoryDto;
+import com.alinesno.infra.base.search.memory.service.AgentMemoryService;
+import com.alinesno.infra.common.facade.response.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 智能体记忆库控制器
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api/base/search/memoryData")
+public class MemoryDataController {
+
+    @Autowired
+    private IMemoryBase memoryService ;
+
+    @Autowired
+    private AgentMemoryService agentMemoryService;
+
+    /**
+     * 查询记忆库
+     */
+    @RequestMapping("/query")
+    public R<String> queryMemoryData(String memoryId) {
+
+        return R.ok("数据查询成功");
+    }
+
+    /**
+     * 增加记忆
+     * @return
+     */
+    @PutMapping("/add")
+    public R<String> addMemoryData(@RequestBody @Validated AgentMemoryDto dto) {
+        agentMemoryService.addMemoryData(dto);
+        return R.ok("数据添加成功");
+    }
+
+    /**
+     * 批量增加记忆
+     * @return
+     */
+    @PutMapping("/addBatch")
+    public R<String> addBatchMemoryData(@RequestBody @Validated List<AgentMemoryDto> dtos) {
+        for(AgentMemoryDto dto : dtos){
+            agentMemoryService.addMemoryData(dto);
+        }
+        return R.ok("数据添加成功");
+    }
+
+    /**
+     * 删除记忆库
+     * @param memoryId
+     * @param data
+     * @return
+     */
+    @DeleteMapping("/delete")
+    public R<String> deleteMemoryData(String memoryId, String data) {
+
+        return R.ok("数据删除成功");
+    }
+
+}
