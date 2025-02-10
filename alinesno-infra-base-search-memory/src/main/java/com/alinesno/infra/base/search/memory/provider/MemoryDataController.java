45a21a7bc12472bdb2597dc292c5a9d3f0e59464
李娜(Allen) <allenAip@gmail.com>
2025-02-11 00:13:08 +0800

    feat:在MemoryDataController中添加日志记录并优化批量添加逻辑

--- alinesno-infra-base-search-memory/src/main/java/com/alinesno/infra/base/search/memory/provider/MemoryDataController.java
+++ alinesno-infra-base-search-memory/src/main/java/com/alinesno/infra/base/search/memory/provider/MemoryDataController.java
@@ -2,7 +2,7 @@ package com.alinesno.infra.base.search.memory.provider;
 
 import com.alinesno.infra.base.search.memory.IMemoryBase;
 import com.alinesno.infra.base.search.memory.bean.AgentMemoryDto;
-import com.alinesno.infra.base.search.memory.service.AgentMemoryService;
+import com.alinesno.infra.base.search.memory.service.IAgentMemoryService;
 import com.alinesno.infra.common.facade.response.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +23,7 @@ public class MemoryDataController {
     private IMemoryBase memoryService ;
 
     @Autowired
-    private AgentMemoryService agentMemoryService;
+    private IAgentMemoryService agentMemoryService;
 
     /**
      * 查询记忆库
@@ -40,7 +40,9 @@ public class MemoryDataController {
      */
     @PutMapping("/add")
     public R<String> addMemoryData(@RequestBody @Validated AgentMemoryDto dto) {
-        agentMemoryService.addMemoryData(dto);
+
+        log.debug("agentId:{},agentName:{}" , dto.getAgentId(), dto.getAgentName());
+        agentMemoryService.addBatchMemoryData(List.of(dto) ,dto);
         return R.ok("数据添加成功");
     }
 
@@ -49,10 +51,22 @@ public class MemoryDataController {
      * @return
      */
     @PutMapping("/addBatch")
-    public R<String> addBatchMemoryData(@RequestBody @Validated List<AgentMemoryDto> dtos) {
+    public R<String> addBatchMemoryData(@RequestBody @Validated List<AgentMemoryDto> dtos ,
+                                        @RequestParam String agentId ,
+                                        @RequestParam String agentName) {
+
+        log.debug("agentId:{},agentName:{}" , agentId , agentName);
+
         for(AgentMemoryDto dto : dtos){
-            agentMemoryService.addMemoryData(dto);
+            dto.setAgentId(Long.parseLong(agentId));
+            dto.setAgentName(agentName);
         }
+
+        AgentMemoryDto dto = new AgentMemoryDto() ;
+        dto.setAgentId(Long.parseLong(agentId)) ;
+        dto.setAgentName(agentName) ;
+
+        agentMemoryService.addBatchMemoryData(dtos , dto);
         return R.ok("数据添加成功");
     }
 
