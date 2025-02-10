d5fc28f5457ecc01131a9352b29ea3e3f26973d4
李娜(Allen) <allenAip@gmail.com>
2025-02-11 00:13:08 +0800

    feat:修改 InformationParser 类，调整解析方法返回类型为 List<InformationBean>

--- alinesno-infra-base-search-memory/src/main/java/com/alinesno/infra/base/search/memory/utils/InformationParser.java
+++ alinesno-infra-base-search-memory/src/main/java/com/alinesno/infra/base/search/memory/utils/InformationParser.java
@@ -2,7 +2,6 @@ package com.alinesno.infra.base.search.memory.utils;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
-import com.alinesno.infra.base.search.memory.bean.EntryBean;
 import com.alinesno.infra.base.search.memory.bean.InformationBean;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,21 +19,10 @@ public class InformationParser {
      * @param content 输入的文本内容
      * @return 包含解析结果的列表
      */
-    public static List<EntryBean> parseContent(String content) {
+    public static List<InformationBean> parseContent(String content) {
         try {
             // 解析JSON字符串到List<EntryBean>
-            List<EntryBean> entries = JSON.parseObject(content, new TypeReference<List<EntryBean>>() {});
-
-            // 处理解析后的数据
-            for (EntryBean entry : entries) {
-                System.out.println("Basis and Process: " + entry.reflection.basisAndProcess);
-                for (InformationBean info : entry.information) {
-                    System.out.println("Sentence ID: " + info.sentenceId + ", Key Information: " + info.keyInformation);
-                }
-            }
-
-            return entries ;
-
+            return JSON.parseObject(content, new TypeReference<List<InformationBean>>() {});
         } catch (Exception e) {
             log.error("解析输入内容并提取相关信息时出错：" + e.getMessage(), e);
             throw new RuntimeException(e);
